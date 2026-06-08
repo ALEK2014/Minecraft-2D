dc67cd88980de4fea759b744d1b319c909e3ac90
Alek <Murderer@Alekwppp.pl>
2026-06-08 19:32:27 +0200

    wyliczanie pozycji x, y dla hotbar

--- interface.py
+++ interface.py
@@ -1,2 +1,11 @@
 import pygame
 from wygenerowanie_terenu import generator_terenu_A
+from settings import ROZMIAR_BLOKU, KOLORY, PRZYPISANIE_KLAWISZY, WYSOKOSC_EKRANU, SZEROKOSC_EKRANU
+
+
+def hotbar_minecraft_mace(ekran):
+    shtm = 736
+    khtm = shtm / 9
+
+    punkt_Maciejax = (shtm - SZEROKOSC_EKRANU) // 2
+    punkt_Maciejay = WYSOKOSC_EKRANU - khtm
